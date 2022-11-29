73d50e7ce6eb58d1ab1e1626ee4adf7d70e95920
phyohan88 <105136249+phyohan88@users.noreply.github.com>
2022-11-29 00:11:20 -0800

    fixed render migration

--- backend/db/migrations/20221122052057-create-review.js
+++ backend/db/migrations/20221122052057-create-review.js
@@ -46,6 +46,6 @@ module.exports = {
     } );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable(options, {});
+    await queryInterface.dropTable(options);
   }
 };
